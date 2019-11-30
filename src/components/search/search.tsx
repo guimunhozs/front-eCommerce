ff84186bf6ff4ed80c25c04aaab52b484c564aa7
guimunhozs <guilhermemunhozsilva@outlook.com>
2019-11-30 19:35:27 -0300

    feat(search): criado componente de busca

---
+++ src/components/search/search.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+type props = {
+  filter: string,
+  handlePress: any,
+}
+
+
+const Search = (props: props) => {
+
+  const [filter, setFilter] = useState('');
+
+  const handleTextSearch = (value: string) => {
+    setFilter(value);
+  }
+
+  const handleSearch = (e: any) => {
+    if (e.which === 13) {
+      props.handlePress(filter);
+    }
+  }
+
+  const handleClear = () => {
+    setFilter('');
+    props.handlePress('');
+  }
+
+  const showButtonClear = () => {
+    return filter.length ? <button id="iconCancel" className="fa fa-times-circle iconSearch" onClick={() => handleClear()} /> : '';
+  }
+
+  return (
+    <div className="searchSection">
+      <i id="iconSearch" className="fa fa-search iconSearch"></i>
+      <input className="inputSearch" type="text" placeholder={ filter? filter : 'O que você precisa?' } onChange={e => handleTextSearch(e.target.value)} value={filter} onKeyPress={(e: any) => handleSearch(e)} />
+      { showButtonClear() }
+    </div>
+  );
+}
+
+export default Search;
