069c5a083a8ad64d139ba763bfd6bcffe7a90bab
guimunhozs <guilhermemunhozsilva@outlook.com>
2019-12-17 00:57:30 -0300

    fix(lib-pagination): melhorada as logicas e regras

--- src/components/pagination/pagination.tsx
+++ src/components/pagination/pagination.tsx
@@ -4,61 +4,88 @@ import { PaginationBox } from "./pagination.css";
 import { pagination } from "./../../libs/calculatePages";
 
 type props = {
-  productQuantity: number,
-  total: number,
-  page: number,
-  handleClick: any,
-}
-
+  limitProductPage: number;
+  numberProducts: number;
+  currentPage: number;
+  handleClick: any;
+};
 
 const Pagination = (props: props) => {
-
   const [numberPage, setNumberPage] = useState(0);
+  const [paginationConfig, setPaginationConfig] = useState({
+    hasDotBefore: false,
+    hasDotAfter: false,
+    rangePagesShow: [] as number[]
+  });
 
   useEffect(() => {
-    setNumberPage(Math.ceil(props.total / props.productQuantity) || 0);
-  }, [props.total, props.page, props.productQuantity]);
+    setNumberPage(
+      Math.ceil(props.numberProducts / props.limitProductPage) || 0
+    );
+    setPaginationConfig(pagination(7, 10));
+    console.log(paginationConfig);
+  }, [props.numberProducts, props.currentPage, props.limitProductPage]);
 
   const pageCalculator = (numPage: number) => {
-    if(numPage<1){
+    if (numPage < 1) {
       return 1;
-    } else if(numPage> numberPage){
+    } else if (numPage > numberPage) {
       return numberPage;
     } else {
       return props.handleClick(numPage);
     }
-  }
+  };
 
   const changePage = (page: number) => {
-    props.handleClick(page)
+    props.handleClick(page);
   };
 
   const handleNumberPagination = () => {
-    const calcPages = numberPage? pagination(props.page, numberPage) : [];
-  
-    return calcPages.map((element, index) => { 
-      if(element === '...') {
-       return <button key={index} className='buttonPage' disabled>{element}</button>
-      } else {
-       return <button key={index} className={'buttonPage ' + (element === props.page ? 'selected' : '')} onClick={() => changePage(+element)}>{element}</button>
-      }
-    });
-
-  }
+    // setPaginationConfig(pagination(props.currentPage, props.totalPages));
+    // console.log(paginationConfig);
+    // const calcPages = paginationConfig.rangePagesShow;
+    // return calcPages.map((element: any, index: any) => {
+    //   if (element === "...") {
+    //     return (
+    //       <button key={index} className="buttonPage" disabled>
+    //         {element}
+    //       </button>
+    //     );
+    //   } else {
+    return (
+      <button key={1} onClick={() => changePage(1)}>
+        {1}
+      </button>
+    );
+    // }
+    // });
+  };
 
   return (
     <div className="productPage">
       <PaginationBox>
-        <button className="fa fa-angle-double-left buttonPage" onClick={() => changePage(1)}></button>
-        <button className="fa fa-angle-left buttonPage" onClick={() => pageCalculator(props.page - 1)}></button>
+        <button
+          className="fa fa-angle-double-left buttonPage"
+          onClick={() => changePage(1)}
+        ></button>
+        <button
+          className="fa fa-angle-left buttonPage"
+          onClick={() => pageCalculator(props.currentPage - 1)}
+        ></button>
 
-        { handleNumberPagination() }
+        {handleNumberPagination()}
 
-        <button className="fa fa-angle-right buttonPage" onClick={() => pageCalculator(props.page + 1)}/>
-        <button className="fa fa-angle-double-right buttonPage" onClick={() => changePage(numberPage)} />
+        <button
+          className="fa fa-angle-right buttonPage"
+          onClick={() => pageCalculator(props.currentPage + 1)}
+        />
+        <button
+          className="fa fa-angle-double-right buttonPage"
+          onClick={() => changePage(numberPage)}
+        />
       </PaginationBox>
     </div>
   );
-}
+};
 
 export default Pagination;
