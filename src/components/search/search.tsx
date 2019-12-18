c3acd738a1929b7910a660b9fd922c86c56ee99b
guimunhozs <guilhermemunhozsilva@outlook.com>
2019-12-17 22:27:21 -0300

    fix(search): criado styled component e adicionado melhorias no codigo

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,41 +1,63 @@
 import React, { useState } from "react";
 
+import {
+  SearchSection,
+  SearchIcon,
+  SearchCancel,
+  SearchInput
+} from "./search.css";
+
 type props = {
-  filter: string,
-  handlePress: any,
+  filter: string;
+  handlePress: any;
+};
+
+enum Keys {
+  Enter = 13
 }
 
-
 const Search = (props: props) => {
-
-  const [filter, setFilter] = useState('');
+  const [filter, setFilter] = useState("");
 
   const handleTextSearch = (value: string) => {
     setFilter(value);
-  }
+  };
 
   const handleSearch = (e: any) => {
-    if (e.which === 13) {
+    if (e.which === Keys.Enter) {
       props.handlePress(filter);
     }
-  }
+  };
 
   const handleClear = () => {
-    setFilter('');
-    props.handlePress('');
-  }
+    setFilter("");
+    props.handlePress("");
+  };
 
   const showButtonClear = () => {
-    return filter.length ? <button id="iconCancel" className="fa fa-times-circle iconSearch" onClick={() => handleClear()} /> : '';
-  }
+    return filter.length ? (
+      <SearchCancel
+        className="fa fa-times-circle"
+        onClick={() => handleClear()}
+      />
+    ) : (
+      ""
+    );
+  };
 
   return (
-    <div className="searchSection">
-      <i id="iconSearch" className="fa fa-search iconSearch"></i>
-      <input className="inputSearch" type="text" placeholder={ filter? filter : 'Produtos' } onChange={e => handleTextSearch(e.target.value)} value={filter} onKeyPress={(e: any) => handleSearch(e)} />
-      { showButtonClear() }
-    </div>
+    <SearchSection>
+      <SearchIcon className="fa fa-search"></SearchIcon>
+      <SearchInput
+        type="text"
+        placeholder={filter ? filter : "Produtos"}
+        onChange={e => handleTextSearch(e.target.value)}
+        value={filter}
+        onKeyPress={(e: any) => handleSearch(e)}
+      />
+      {showButtonClear()}
+    </SearchSection>
   );
-}
+};
 
 export default Search;
